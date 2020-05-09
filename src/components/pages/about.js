4dc669e8fca9b26919c0cba6dff3d0c10e4ef5f3
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-05-09 17:00:06 -0500

    last commit after crash course todo list tutorial

---
+++ src/components/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function about() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>this is the todoList app v1.0.0. 
+                It is part of a React crash course</p>
+        </React.Fragment>
+    )
+}
+
+export default about;
